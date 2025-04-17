ec094be67ae7f85cac6c875e612bd0486ef3b142
Kavindu Dilhara Sannasgala <kavindu_2020691@fot.ruh.ac.lk>
2025-04-17 22:59:39 +0530

    Initial commit

---
+++ src/services/productService.tsx
@@ -0,0 +1,78 @@
+export const fetchProducts = async (token: string | null) => {
+try {
+    const response = await fetch(`${import.meta.env.VITE_API_URL}/product/all-products`, {
+    headers: {
+        'Authorization': `Bearer ${token}`
+    }
+    });
+    const data = await response.json();
+    if (data.status === "SUCCESS") {
+    return data.data;
+    }
+} catch (error) {
+    console.error('Error fetching products:', error);
+}
+return [];
+};
+
+export const addProduct = async (productData: FormData, token: string | null) => {
+    try {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/product/add-product`, {
+        method: 'POST',
+        body: productData,
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      });
+      const result = await response.json();
+      if (result.status === "SUCCESS") {
+        return result.data;
+      } else {
+        console.error('Failed to add product:', result.message);
+      }
+    } catch (error) {
+      console.error('Error adding product:', error);
+    }
+    return null;
+  };
+
+  export const updateProduct = async (productId: string, productData: FormData, token: string | null) => {
+    try {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/product/update-product/${productId}`, {
+        method: 'PUT',
+        body: productData,
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      });
+      const result = await response.json();
+      if (result.status === "SUCCESS") {
+        return result.data;
+      } else {
+        console.error('Failed to update product:', result.message);
+      }
+    } catch (error) {
+      console.error('Error updating product:', error);
+    }
+    return null;
+  };
+
+export const deleteProduct = async (productId: string, token: string | null) => {
+try {
+    const response = await fetch(`${import.meta.env.VITE_API_URL}/product/delete-product/${productId}`, {
+    method: 'DELETE',
+    headers: {
+        'Authorization': `Bearer ${token}`
+    }
+    });
+    const data = await response.json();
+    if (data.status === "SUCCESS") {
+    return true;
+    } else {
+    console.error('Failed to delete product:', data.message);
+    }
+} catch (error) {
+    console.error('Error deleting product:', error);
+}
+return false;
+};
